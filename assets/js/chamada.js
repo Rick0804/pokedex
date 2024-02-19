2a7a10418afb00af1c0c8ced9e0ef45eb91f2a0b
henrique <henriquenunesmagalha@gmail.com>
2024-02-19 00:47:34 -0300

    first commit

---
+++ assets/js/chamada.js
@@ -0,0 +1,207 @@
+//quantos pokemons vão aparecer 
+//qual pack vai aparecer
+
+const pokeApi = {};
+
+pokeApi.chamadaApi = (limit, offset) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/?limit=${limit}&offset=${offset}`;
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => new Error("erro de requisição" + error.status))
+}
+
+pokeApi.pokeType = (name) => {
+    const urlNome = `https://pokeapi.co/api/v2/pokemon/${name}/`
+    return fetch(urlNome)
+        .then((response) => response.json())
+        .catch((error) => new Error("erro de requisição " + error));
+}
+
+creatElementList = () => {
+    let li = document.createElement('li');
+    return li;
+}
+
+creatElementDiv = () => {
+    const div = document.createElement('div');
+    return div;
+}
+
+creatElementDiv = () => {
+    const div = document.createElement('div');
+    return div;
+}
+
+adicionaElement = (name, type, img, ability) => {
+    const li = creatElementList();
+    const divine = creatElementDiv();
+    div.appendChild(li);
+    li.classList.add('poke')
+    switch (type) {
+        case 'grass':
+            li.classList.add("grass")
+            divine.innerHTML = "dafajda"
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'bug':
+            li.classList.add('bug')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'dark':
+            li.classList.add('dark')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'water':
+            li.classList.add('water')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'dragon':
+            li.classList.add('dragon')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'electric':
+            li.classList.add('electric')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'fairy':
+            li.classList.add('fairy')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'ghost':
+            li.classList.add('ghost')
+            li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`;
+            break;
+        case 'fighting':
+            li.classList.add('fighting')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'fire':
+            li.classList.add('fire')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'ice':
+            li.classList.add('ice')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'poison':
+            li.classList.add('poison')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'flying':
+            li.classList.add('flying')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'normal':
+            li.classList.add('normal')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'ground':
+
+            li.classList.add('ground')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'steel':
+            li.classList.add('steel')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'psychic':
+            li.classList.add('psychic')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        case 'rock':
+            li.classList.add('rock')
+            li.innerHTML = li.innerHTML = `<div class="chars">
+                                <p>${name[0].toUpperCase() + name.substring(1)}</p>
+                                <p>${type.toUpperCase()}</p>
+                                <p>${ability.toUpperCase()}</p>
+                            </div>    
+                <img src="${img}" alt="" srcset="">`
+            break;
+        default: li.innerHTML = name + " " + type + " " + img;;
+    }
+}
---
+++ assets/js/controller.js
@@ -0,0 +1,29 @@
+let limit = 20;
+let offset = 0;
+const div = document.querySelector(".lista_pokemon");
+
+/*
+para pegar o tipo do pokemon é results.types[0].type.name
+para pegar a imagem do pokemon é results.sprites.front_default
+*/
+
+
+
+document.querySelector(".btn").addEventListener('click', () => {
+    pokeApi.chamadaApi(limit, offset).then((response) => {
+        response.results.forEach(pokemon => {
+            pokeApi.pokeType(pokemon.name)
+                .then((results) => {
+                    const img = results.sprites.front_default;
+                    const type = results.types[0].type.name;
+                    const ability = results.abilities[0].ability.name;
+                    adicionaElement(pokemon.name, type, img, ability)
+                });
+        });
+
+    });
+    offset += limit;
+})
+
+
+
